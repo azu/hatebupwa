424667bec7c1a1ec9657f434e173fc2db4e36058
azu <azuciao@gmail.com>
2018-04-11 12:36:13 +0900

    fix(sw): add navigateFallback

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,6 @@
 module.exports = {
     clientsClaim: true,
+    navigateFallback: "/index.html",
     globDirectory: "build/",
     globPatterns: ["**/*.{json,ico,png,html,js,css}"],
     swDest: "build/sw.js"
