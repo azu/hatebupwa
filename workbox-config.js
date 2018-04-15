9ff151fb20481ec44b1e8ac855c31625f56256c0
azu <azuciao@gmail.com>
2018-04-15 18:30:24 +0900

    feat(sw): add runtime cache

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,23 @@
+// https://developers.google.com/web/tools/workbox/guides/configure-workbox
 module.exports = {
     clientsClaim: true,
+    // For routing
     navigateFallback: "/index.html",
     globDirectory: "build/",
     globPatterns: ["**/*.{json,ico,png,html,js,css}"],
-    swDest: "build/sw.js"
+    swDest: "build/sw.js",
+    runtimeCaching: [
+        {
+            // Match any request ends with .png, .jpg, .jpeg or .svg.
+            urlPattern: /\.(?:png|jpg|jpeg|svg|woff|woff2)$/,
+            // Apply a cache-first strategy.
+            handler: "cacheFirst",
+            options: {
+                // Only cache 10 images.
+                expiration: {
+                    maxEntries: 10
+                }
+            }
+        }
+    ]
 };
